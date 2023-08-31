012364cd0c3ff0a6084bc39f7af658b2b6e0baa6
jm12312 <143526574+jm12312@users.noreply.github.com>
2023-08-31 21:04:52 +0530

    Add files via upload

--- 1.1.py
+++ 1.1.py
@@ -1,8 +1,5 @@
 jumbled_superheros = ["DocTor_sTRANGE", "sPiderMan", "MoON_KnigHT", "capTAIN_aMERIca", "hULK"]
 l1 = enumerate(jumbled_superheros)
-
-print(list(l1))
-
 indices = []
 decoded_names = []
 
@@ -11,14 +8,11 @@ for count, element in enumerate(jumbled_superheros, 1):
     indices.append(count)
     element = element.replace("_", " ")
     element = element.lower()
-
     decoded_names.append(element)
-    # name_lengths.append(strlen(element))
 
 name_lengths = list(map(strlen, decoded_names))
+#print(name_lengths)
 
-
-# print(name_lengths)
 
 def length(n):
     return len(n)
@@ -26,16 +20,9 @@ def length(n):
 
 name_lengths.sort()
 decoded_names.sort(key=length)
-"""
-for i in range(5):
-    for j in range(i, 5):
-        if len(decoded_names[i]) > len(decoded_names[j]):
-            c = decoded_names[i]
-            decoded_names[i] = decoded_names[j]
-            decoded_names[j] = c
-"""
+
 for i in range(5):
     decoded_names[i] = decoded_names[i].title()
 
 for i in range(5):
-    print(indices[i], decoded_names[i])
+    print(str(indices[i])+".", decoded_names[i])
